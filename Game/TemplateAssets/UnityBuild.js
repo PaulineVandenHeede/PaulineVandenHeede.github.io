c4a039fd3336e6a3b05dfbfa14c6138180eeecf5
PaulineVandenHeede <vandenheede.pauline@gmail.com>
2022-11-09 10:26:54 +0100

    mobile test 7

--- Game/TemplateAssets/UnityBuild.js
+++ Game/TemplateAssets/UnityBuild.js
@@ -30,11 +30,11 @@ updateBannerVisibility();
 }
 
 var buildUrl = "Build";
-var loaderUrl = buildUrl + "/TEST_MOBILE_08.loader.js";
+var loaderUrl = buildUrl + "/TEST_MOBILE_09.loader.js";
 var config = {
-dataUrl: buildUrl + "/TEST_MOBILE_08.data.unityweb",
-frameworkUrl: buildUrl + "/TEST_MOBILE_08.framework.js.unityweb",
-codeUrl: buildUrl + "/TEST_MOBILE_08.wasm.unityweb",
+dataUrl: buildUrl + "/TEST_MOBILE_09.data.unityweb",
+frameworkUrl: buildUrl + "/TEST_MOBILE_09.framework.js.unityweb",
+codeUrl: buildUrl + "/TEST_MOBILE_09.wasm.unityweb",
 streamingAssetsUrl: "StreamingAssets",
 companyName: "DefaultCompany",
 productName: "Grp12_Ketnet",
