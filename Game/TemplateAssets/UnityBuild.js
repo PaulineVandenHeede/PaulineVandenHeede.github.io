d6ac264925b603e03056ea7aa3e878faa648da68
PaulineVandenHeede <vandenheede.pauline@gmail.com>
2022-11-23 15:11:46 +0100

    rescaling ui

--- Game/TemplateAssets/UnityBuild.js
+++ Game/TemplateAssets/UnityBuild.js
@@ -30,11 +30,11 @@ updateBannerVisibility();
 }
 
 var buildUrl = "Build";
-var loaderUrl = buildUrl + "/TEST_MULTITOUCH_JS_05.loader.js";
+var loaderUrl = buildUrl + "/TEST_RESCALING_UI_01.loader.js";
 var config = {
-dataUrl: buildUrl + "/TEST_MULTITOUCH_JS_05.data.unityweb",
-frameworkUrl: buildUrl + "/TEST_MULTITOUCH_JS_05.framework.js.unityweb",
-codeUrl: buildUrl + "/TEST_MULTITOUCH_JS_05.wasm.unityweb",
+dataUrl: buildUrl + "/TEST_RESCALING_UI_01.data.unityweb",
+frameworkUrl: buildUrl + "/TEST_RESCALING_UI_01.framework.js.unityweb",
+codeUrl: buildUrl + "/TEST_RESCALING_UI_01.wasm.unityweb",
 streamingAssetsUrl: "StreamingAssets",
 companyName: "DefaultCompany",
 productName: "Grp12_Ketnet",
